ba7bea91db5de2519e7e5516d8b8b80030b8924f
Jayamini Liyanage <128564267+JayaminiCL@users.noreply.github.com>
2024-04-05 12:53:07 +1100

    estimateCorMat.R

--- R/estimateCorMat.R
+++ R/estimateCorMat.R
@@ -1,11 +1,13 @@
 estimateCorMat <- function(Y,shrinkCor=TRUE){
-  N <- ncol(Y)
-  K <- nrow(Y)
+  N <- ncol(Y) # NO.OF METABOLITES/OUTCOMES
+  K <- nrow(Y) # NO.OF STUDIES
+  # IF THE NO.OF STUDIES ARE LESS THAN OR EQUAL TO 2, SET BETWEEN-STUDY CORRELATIONS TO BE ZERO
   if (K <= 2){
     cormat <- 0
   }else {
     if (N > K){
       if(shrinkCor){
+        # ESTIMATE CORRELATIONS VIA SHRINKAGE
         cormat <- cor.shrink(Y,verbose = FALSE)[1:N,1:N]
       }else{
         cormat <- cor(Y)
